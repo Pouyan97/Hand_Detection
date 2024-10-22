80b83a36089607eca1249f5d593620ebac321b55
pouyan97 <sppf75@gmail.com>
2024-10-22 13:11:12 -0500

    argument based pipeline populating script

--- scripts/populate_pipeline.py
+++ scripts/populate_pipeline.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from pose_pipeline.utils.jupyter import play,play_grid
 from pose_pipeline.pipeline import BlurredVideo,LiftingPerson,LiftingMethod,TopDownPerson,Video
@@ -6,8 +7,8 @@ from multi_camera.datajoint.sessions import Recording
 from hand_detection.hand_dj import HandPoseReconstructionMethodLookup,HandPoseReconstructionMethod, HandPoseReconstruction,HandPoseEstimationMethodLookup
 from hand_detection.hand_dj import HandBbox,HandBboxMethod, HandPoseEstimation,HandPoseEstimationMethod,HandPoseEstimationVideo,MJXReconstruction
 
-from hand_detection.hand_dj import schema
-schema.jobs.delete()
+# from hand_detection.hand_dj import schema
+# schema.jobs.delete()
 
 def populate_hand_bbox(keys, detection_methods = [1]):
     for detection_method in detection_methods:
@@ -46,25 +47,47 @@ def populate_hand_reconstruction(keys, detection_methods=[1], estimation_methods
                 HandPoseReconstruction.populate(keys,reserve_jobs=True)
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Populate hand bbox, estimation, and reconstruction.')
+    parser.add_argument('--participants', type=str, nargs='+', required=True, help='List of participants')
+    parser.add_argument('--filenames', type=str, nargs='+', required=True, help='List of filenames')
+    parser.add_argument('--detection_methods', type=int, nargs='+', default=[1,2], help='Detection methods')
+    parser.add_argument('--estimation_methods', type=int, nargs='+', default=[-1,0,1], help='Estimation methods')
+    parser.add_argument('--reconstruction_methods', type=int, nargs='+', default=[3], help='Reconstruction methods')
+    parser.add_argument('--populate', choices=['bbox', 'estimation', 'reconstruction', 'all'], required=True, help='Choose what to populate')
 
-participants = ["yj843","rko5c","8wj64","lgtfc"]
-filenames = ['_40angle%']
-for participant_id in participants:
-    for filename in filenames:
-        # participant_id= participants[1]
-        # participant_videos = (Recording & f'participant_id="{participant_id}"').fetch('KEY')
-        participant_videos = (Recording & (SingleCameraVideo & f'filename LIKE "{participant_id}{filename}"')).fetch('KEY')
-        detection_methods = [1]
-        estimation_methods = [-1,0,1,3,4]
-        reconstruction_methods=[3]
+    args = parser.parse_args()
+    
+    # participants = ["yj843","rko5c","8wj64","lgtfc","oxbcl"]
+    # participants = ["oxbcl"]
+    # filenames = ['_%']
+    # detection_methods = [2]
+    # estimation_methods = [-1,0,1,2,3,4]
+    # reconstruction_methods=[3]
+    participants = args.participants
+    filenames = args.filenames
+    detection_methods = args.detection_methods
+    estimation_methods = args.estimation_methods
+    reconstruction_methods = args.reconstruction_methods
+    populate = args.populate
 
-        for rk in participant_videos:
-            keys = (SingleCameraVideo & rk ).fetch('KEY')
-            print("Processing", len(keys), "Keys")
-            populate_hand_bbox(keys, detection_methods = detection_methods)
-            populate_hand_estimation(keys,  detection_methods= detection_methods, estimation_methods = estimation_methods)
-            # populate pose reconstruction as well       
-            key = (CalibratedRecording & (HandPoseEstimation & keys)).fetch('KEY')
-            populate_hand_reconstruction(key, detection_methods = detection_methods, 
-                                        estimation_methods = estimation_methods, 
-                                        reconstruction_methods = reconstruction_methods)
+    for participant_id in participants:
+        for filename in filenames:
+            # participant_videos = (Recording & f'participant_id="{participant_id}"').fetch('KEY')
+            participant_videos = (Recording & (SingleCameraVideo & f'filename LIKE "{participant_id}%{filename}%"')).fetch('KEY')
+            for rk in participant_videos:
+                keys = (SingleCameraVideo & rk ).fetch('KEY')
+                print("Processing", len(keys), "Keys")
+                if populate == 'bbox' or populate == 'all':
+                    populate_hand_bbox(keys, detection_methods = detection_methods)
+                if populate == 'estimation' or populate == 'all':
+                    populate_hand_estimation(keys,  detection_methods= detection_methods, estimation_methods = estimation_methods)
+                # populate pose reconstruction as well       
+                if populate == 'reconstruction' and populate == 'all':
+                    key = (CalibratedRecording & (HandPoseEstimation & keys)).fetch('KEY')
+                    populate_hand_reconstruction(key, detection_methods = detection_methods, 
+                                                estimation_methods = estimation_methods, 
+                                                reconstruction_methods = reconstruction_methods)
+
+if __name__ == '__main__':
+    main()
